d97219ea7963d39f8d8f4be8659355c7738e7163
Diego Pagini <diego.pagini@met.ey.com>
2021-11-11 09:37:19 +0100

    tabs panel

--- au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
+++ au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
@@ -14,26 +14,31 @@ import { AuTabComponent } from "../au-tab/au-tab.component";
   styleUrls: ["../tab-panel.component.scss"],
 })
 export class AuTabPanelComponent implements AfterContentInit {
-  @ContentChildren(AuTabComponent)
-  tabs: QueryList<AuTabComponent>;
+  // ContentChildren decorator allows you to query for the tabs that are
+  @ContentChildren(AuTabComponent) // The type has of the ContentChildren has to be specified
+  tabs: QueryList<AuTabComponent>; // QueryList is a generic type that represents a list of a certain type. In this case AuTabComponent
 
   @Input()
-  headerTemplate: TemplateRef<any>;
+  headerTemplate: TemplateRef<any>; // this is a reference to the headerTemplate
 
+  // Because we are using the ContentChildren decorator, we need to implement the AfterContentInit interface
   ngAfterContentInit() {
+    // we are locking for a tab with selected property set to true
     const selectedTab = this.tabs.find((tab) => tab.selected);
-
+    // if there is no selected tab, we select the first one
     if (!selectedTab && this.tabs.first) {
       this.tabs.first.selected = true;
     }
   }
 
   selectTab(tab: AuTabComponent) {
+    // we put all the tabs on false
     this.tabs.forEach((tab) => (tab.selected = false));
-
+    // and them we set the selected tab to true
     tab.selected = true;
   }
 
+  // this method returns the header with the contenxt of the tab
   get tabsContext() {
     return {
       tabs: this.tabs,
